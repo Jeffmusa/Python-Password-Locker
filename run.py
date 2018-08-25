2ffb9041a9ba6cc51160c97cc66cd7a95a69f193
Jeff Musa <jeffmusa@gmail.com>
2018-08-25 17:11:36 +0300

    credenrials run

--- run.py
+++ run.py
@@ -42,6 +42,45 @@ def display_accounts():
  #_____________________________________________________________________________________________________________
 
 
+def create_credentials(credentials_name,user_name,password,email):
+    '''
+    Function to create a new account
+    '''
+    new_credentials = Credentials(credentials_name,user_name,password,email)
+    return new_credentials
+
+def save_credentials(credentials):
+    '''
+    Function to save account
+    '''
+    credentials.save_credentials()    
+
+def del_credentials(credentials):
+    '''
+    Function to delete a account
+    '''
+    credentials.delete_credentials()    
+
+
+def find_credentials(name):
+    '''
+    Function that finds a account by nane and returns the account
+    '''
+    return Credentials.find_by_name(name)    
+
+def check_existing_credentials(name):
+    '''
+    Function that check if an account exists with that name and return a Boolean
+    '''
+    return Credentials.credentials_exist(name)    
+
+def display_credentials():  #CHECK HERE LATER
+    '''
+    Function that returns all the saved accounts
+    '''
+    return Credentials.display_credentials() 
+
+
 
 
 def main():
